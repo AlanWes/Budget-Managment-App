132b888a22d175cb8621ce62bcc4ed6af63aaf22
alanwes <125877927+AlanWes@users.noreply.github.com>
2023-03-14 18:42:43 +0100

    final_register_login_sites

--- src_projects/Project_Budget/Project_Budget/urls.py
+++ src_projects/Project_Budget/Project_Budget/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from login_register_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    ################################
+    path('',views.SignupPage,name='signup'),
+    path('login/',views.LoginPage,name='login')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
