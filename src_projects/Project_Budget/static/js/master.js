6f0eb0e048eebea6227ccbab7b2721738902c9ad
alanwes <125877927+AlanWes@users.noreply.github.com>
2023-03-31 21:18:56 +0200

    change-style-master

--- src_projects/Project_Budget/static/js/master.js
+++ src_projects/Project_Budget/static/js/master.js
@@ -1,8 +1,22 @@
 /// FOR CSS
 
+///MOBILE MENU BTN
+
 const menubtn = document.querySelector(".menu-btn")
 const navLinks = document.querySelector(".nav-links")
 
 menubtn.addEventListener('click',()=>{
 navLinks.classList.toggle('mobile-menu')
 })
+
+///SCROLL EFFECT
+
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
