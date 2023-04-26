b951ed3f0832334f8a33533a173f2b44dbefcb07
alanwes <125877927+AlanWes@users.noreply.github.com>
2023-04-26 22:15:20 +0200

    think about goals site

--- src_projects/Project_Budget/static/js/goals.js
+++ src_projects/Project_Budget/static/js/goals.js
@@ -8,3 +8,21 @@ const navLinks = document.querySelector(".nav-links")
 menubtn.addEventListener('click',()=>{
 navLinks.classList.toggle('mobile-menu')
 })
+
+///GOALS
+
+const goalsForm = document.querySelector('.goals form');
+const goalsDiv = document.querySelector('.goals');
+const goalsAfterDiv = document.querySelector('.goals-after');
+
+goalsForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+  goalsDiv.classList.toggle('hidden');
+  goalsAfterDiv.classList.toggle('hidden');
+  const savingsGoalText = document.querySelector('#savings-goal-text');
+  savingsGoalText.innerText = document.querySelector('#savings-goal').value;
+  const debtGoalText = document.querySelector('#debt-goal-text');
+  debtGoalText.innerText = document.querySelector('#debt-goal').value;
+  const investmentGoalText = document.querySelector('#investment-goal-text');
+  investmentGoalText.innerText = document.querySelector('#investment-goal').value;
+});
