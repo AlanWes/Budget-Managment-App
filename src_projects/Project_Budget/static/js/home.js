b215c564831a8bd212cde08802bf5e6631aa0fd2
alanwes <125877927+AlanWes@users.noreply.github.com>
2023-05-17 01:25:57 +0200

    big style changes

--- src_projects/Project_Budget/static/js/home.js
+++ src_projects/Project_Budget/static/js/home.js
@@ -4,9 +4,23 @@
 
 const menubtn = document.querySelector(".menu-btn")
 const navLinks = document.querySelector(".nav-links")
+const menubtn2 = document.querySelector(".menu-btn-after")
+const body = document.body;
 
 menubtn.addEventListener('click',()=>{
-navLinks.classList.toggle('mobile-menu')
+  body.style.overflow = 'hidden';
+  menubtn.style.display = "none";
+  menubtn2.style.display = "block";
+  navLinks.classList.toggle('mobile-menu')
+  navLinks.classList.remove('mobile-menu2');
+})
+
+menubtn2.addEventListener('click',()=>{
+  body.style.overflow = 'auto';
+  menubtn.style.display = "block";
+  menubtn2.style.display = "none";
+  navLinks.classList.toggle('mobile-menu2')
+  navLinks.classList.remove('mobile-menu');
 })
 
 ///CHANGE MONEY DISPLAY
